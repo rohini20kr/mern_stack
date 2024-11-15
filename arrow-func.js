48feb2fdb2caa13f9a6481fe51677816912ea97f
rohini20kr <rohinirajesh20@gmail.com>
2024-11-15 10:23:35 +0530

    Day-1 to Day-4 Commit

---
+++ arrow-func.js
@@ -0,0 +1,13 @@
+//function declaration
+function greet(name){
+    return "Hello,"+name;
+}
+//function call
+console.log(greet("Rohini"));
+console.log(greet("Mamatha"));
+
+// function sum(a,b) {
+//     return a+b;
+// }
+const sum =(a,b) =>a+b;
+console.log(sum(10,20));
---
+++ map-filter-reduce.js
@@ -0,0 +1,14 @@
+//map method
+const num =[1,2,3,4,5];
+const doubled = num.map(num => num *2);
+console.log(doubled);
+
+//filter method
+const filter_num =[1,2,3,4,5];
+const evennum = filter_num.filter(num => num %2 == 0);
+console.log(evennum);
+
+//reduce method 
+const sum_num =[1,2,3,4,5];
+const sum = sum_num.reduce((accumulator , currentValue)=> accumulator;
+console.log(sum);
